aee25a88e9cada7346cad31373c8097ac6337387
Parikshit Hooda <phooda804@live.com>
2021-02-25 15:21:50 +0530

    init commit

--- foreground.js
+++
@@ -1,9 +0,0 @@
-console.log('from fg');
-
-let result = document.querySelector('#movie_player > div.ytp-chrome-bottom > div.ytp-progress-bar-container > div.ytp-progress-bar').getAttribute("aria-valuetext");
-
-console.log(result);
-
-// chrome.runtime.sendMessage({ timestring: result }, function(response) {
-//     console.log('response received' + response);
-// });
